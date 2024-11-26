b41d0a1ed7b5fae6ed22bb9019c735c09d837b12
apkhoror <sasukesenda@gmail.com>
2024-11-26 18:35:30 +0700

    mengubah apiURL countAPI

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-const apiURL = "./data.json";
+const apiURL = "https://api.countapi.xyz/hit/apkhoror.github.io/social-links";
 
 // Fetch data dari CountAPI
 fetch(apiURL, {
   method: "GET",
-  mode: "no-cors",
 })
   .then((response) => response.json())
   .then((data) => {
