30ddc48875f085eca714e51dc504de2ceef91464
apkhoror <122208341+apkhoror@users.noreply.github.com>
2024-11-26 19:50:55 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,1 @@
-const apiURL = "https://api.countapi.xyz/hit/apkhoror.github.io/social-links";
 
-// Fetch data dari CountAPI
-fetch(apiURL, {
-  method: "GET",
-  mode: "no-cors",
-})
-  .then((response) => response)
-  .then((data) => {
-    document.getElementById("visitorCount").textContent = data.value;
-  })
-  .catch((err) => {
-    console.log("Error Fetching data: ", err);
-  });
