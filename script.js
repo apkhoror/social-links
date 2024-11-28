46141d997dd3750500730e6472812e92dbedeb4e
apkhoror <sasukesenda@gmail.com>
2024-11-28 08:49:22 +0700

    update

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 const popup = document.querySelector(".popup");
+const popupContainer = document.querySelector(".container");
 const donasi = function (e) {
   e.preventDefault();
 
   popup.classList.add("show");
+  popupContainer.classList.add("show");
   document.body.classList.add("no-scroll");
 };
 
 popup.addEventListener("click", function () {
   popup.classList.remove("show");
+  popupContainer.classList.remove("show");
   document.body.classList.remove("no-scroll");
 });
